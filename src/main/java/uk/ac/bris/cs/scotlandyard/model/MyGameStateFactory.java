4fa7a7148e8d62c22789826462d666bd38d35f68
finnimo <jehoang17@gmail.com>
2026-03-25 18:55:22 +0000

    - if moves in setup is empty, throw exception - checked for detective being null and or empty
    - now working on comparing graph given to standard graph

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -2,7 +2,11 @@ package uk.ac.bris.cs.scotlandyard.model;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
+
+import java.io.IOException;
 import java.util.*;
+
+import com.google.common.graph.ImmutableValueGraph;
 import jakarta.annotation.Nonnull;
 
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
@@ -10,6 +14,11 @@ import uk.ac.bris.cs.scotlandyard.model.Move.*;
 import uk.ac.bris.cs.scotlandyard.model.Piece.*;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.*;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+// imported graph to check against default graph
+import static io.atlassian.fugue.Iterables.size;
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.readGraph;
+import static uk.ac.bris.cs.scotlandyard.model.ScotlandYard.standardGraph;
 
 /**
  * cw-model
@@ -35,29 +44,51 @@ public final class MyGameStateFactory implements Factory<GameState> {
 		private ImmutableSet<Piece> winner;
 
 		@Override public GameSetup getSetup() {
-			return setup;
+			return this.setup;
 		}
 		@Override  public ImmutableSet<Piece> getPlayers() { return null; }
 		@Override public GameState advance(Move move) {  return null;  }
 		@Override public Optional<Integer> getDetectiveLocation(Detective detective) {return null;}
 		@Override public Optional<TicketBoard> getPlayerTickets(Piece piece) {return null;}
-		@Override public ImmutableList<LogEntry> getMrXTravelLog(){
-			return log;
-		}
-		@Override public ImmutableSet<Piece> getWinner(){return null;}
+		@Override public ImmutableList<LogEntry> getMrXTravelLog(){return log;}
+		@Override public ImmutableSet<Piece> getWinner(){return this.winner;}
 		@Override public ImmutableSet<Move> getAvailableMoves(){return null;}
 
+		// constructor
 		private MyGameState(
 				final GameSetup setup,
 				final ImmutableSet<Piece> remaining,
 				final ImmutableList<LogEntry> log,
 				final Player mrX,
-				final List<Player> detectives){
-			this.setup = setup;
+				final List<Player> detectives) {
+			// check that parameters handed over aren't null
+			if (mrX == null || detectives == null || setup == null) {
+				throw new NullPointerException("Build has been passed null arguments.");
+			}
+			// checks if any list structures handed over are empty
+			else if (remaining.isEmpty() || log.isEmpty() || detectives.isEmpty()){
+				throw new IllegalArgumentException("Build has been passed empty arguments.");
+			}
+			// check empty moves throw
+			if(setup.moves.isEmpty()) throw new IllegalArgumentException("Moves is empty!");
+
+			// initializing variables
+			/*
+			ImmutableValueGraph<Integer, ImmutableSet<Transport>> standardGraph;
+			try {
+				standardGraph = standardGraph();
+			} catch (IOException e) {
+				System.out.println("Standard graph not found. IO error.");
+			}
+			this.setup = new GameSetup(standardGraph, ImmutableList.of(true, false, true, true));
 			this.remaining = remaining;
 			this.log = log;
 			this.mrX = mrX;
 			this.detectives = detectives;
+			// this.winner = new ImmutableSet<>();*/
+
+			// test for duplicate mrXs
+
 
 			// TODO
 			// Need to implement the following checks/tests
@@ -68,15 +99,6 @@ public final class MyGameStateFactory implements Factory<GameState> {
 			// testSwappedMrXShouldThrow <- idk what this means
 			// check that mrX has double tickets
 			// Check that detectives have secret tickets
-			// Check for empty moves
-			// Check for empty graph
-			//
-
-			if (setup == null || remaining.isEmpty() || log.isEmpty() || mrX == null || detectives.isEmpty()){
-				throw new IllegalArgumentException("Arguments cant be null.");
-
-			}
-
 
 
 
